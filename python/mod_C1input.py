fa728f24163a42c2b055ebee00116be7b589b290
+Brendan Lyons <lyonsbc@irisa.cluster>
2016-04-18 15:53:02 -0700

    Can now modify C1input file from default via keyword in autoC1, calling new mod_C1input function.  Fixed bug in reading p-file for profile_omega.Ctor.  User can now interactively cahnge center and width while extending profiles.

---
+++ python/mod_C1input.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 14:29:38 2016
+
+@author: lyonsbc
+"""
+
+import numpy as np
+from my_shutil import mv
+
+def mod_C1input(C1inputs=None,folder='.',append=True):
+    
+    if C1inputs is None:
+        return
+
+    print 'Modifying C1input file'
+    
+    C1copy = C1inputs.copy()
+    
+    new = folder + '/C1input'
+    old = folder + '/C1input.old'
+    mv(new,old)
+    
+    with open(old,'r') as fin:
+        with open(new,'w') as fout:
+        
+            for line in fin:   
+                
+                spl = line.split("=",1)
+
+                if np.size(spl) == 2:
+                    for key in C1copy:
+                        if key.strip() == spl[0].strip():
+                            val = str(C1copy[key]).strip()
+                            C1copy[key]=None
+                            print "\tChanging " + spl[0].strip() + " from " + spl[1].strip() + " to " + val.strip()
+                            #spl[1] = val.rjust(len(spl[1])-1)
+                            spl[1] = "  " + val
+                            line = spl[0] + "=" + spl[1] + "\n"
+                
+                if line.strip() != '/':
+                    fout.write(line)
+          
+            if append:
+                for key in C1copy:
+                    val = C1copy[key]
+                    if val is not None:
+                        line = "\t"+str(key).strip()+"  =  "+str(val).strip()+"\n"
+                        print "\tAppending " + line.strip()
+                        fout.write('\n')
+                        fout.write(line)
+                fout.write('\n')
+                    
+            fout.write(' /\n')
+            
+    return
